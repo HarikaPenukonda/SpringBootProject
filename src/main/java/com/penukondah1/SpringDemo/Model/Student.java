a3bbc3bcec31c215f77d7db0ec3309e694691ab8
harika penukonda <penukondaharika@gmail.com>
2020-07-08 14:27:58 -0500

    Update Student.java

--- src/main/java/com/penukondah1/SpringDemo/Model/Student.java
+++ src/main/java/com/penukondah1/SpringDemo/Model/Student.java
@@ -3,13 +3,12 @@ package com.penukondah1.SpringDemo.Model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity /*specifies that the class is an entity and is mapped to a database table
-*/
+@Entity /*specifies that the class is an entity and is mapped to a database table*/
 public class Student {
 	
 	@Id /*marks a field in a model class as the Primary key*/
-	private String sid;
-	private String sname;
+	private String id;
+	private String name;
 	private String dept;
 	
 	/*A no-arg constructor, so that the object is easier to initialize*/
@@ -18,34 +17,38 @@ public class Student {
 	}
 	
 	/* A parameterized contructor which takes the arguments and intializes the object */
-	public Student(String sid, String sname, String dept) {
+	public Student(String id, String name, String dept) {
 		super();
-		this.sid = sid;
-		this.sname = sname;
+		this.id = id;
+		this.name = name;
 		this.dept = dept;
 	}
-	
-	
-	
-	public String getSid() {
-		return sid;
+
+	public String getId() {
+		return id;
 	}
 
-	public void setSid(String sid) {
-		this.sid = sid;
+	public void setId(String id) {
+		this.id = id;
 	}
 
-	public String getSname() {
-		return sname;
+	public String getName() {
+		return name;
 	}
-	public void setSname(String sname) {
-		this.sname = sname;
+
+	public void setName(String name) {
+		this.name = name;
 	}
+
 	public String getDept() {
 		return dept;
 	}
+
 	public void setDept(String dept) {
 		this.dept = dept;
 	}
+	
+	
+	
 
 }
